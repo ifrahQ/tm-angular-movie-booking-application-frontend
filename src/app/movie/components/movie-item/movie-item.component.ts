5047e84057e5debd9035a734f131e68b064ee3e0
Kumar Abhinav <abhinav0441@gmail.com>
2020-09-01 17:27:56 +0530

    services

--- src/app/movie/components/movie-item/movie-item.component.ts
+++ src/app/movie/components/movie-item/movie-item.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-movie-item',
   templateUrl: './movie-item.component.html',
   styleUrls: ['./movie-item.component.css']
 })
-export class MovieItemComponent implements OnInit {
+export class MovieItemComponent implements OnInit, OnChanges, OnDestroy {
   @Input() movie;
   @Output() rateUpBtnClick = new EventEmitter();
   @Output() rateDownBtnClick = new EventEmitter();
@@ -14,6 +14,15 @@ export class MovieItemComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log('on init');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('on changes', changes);
+  }
+
+  ngOnDestroy() {
+    console.log('on destroy');
   }
 
   onRateUpBtnClick(movie) {
@@ -25,7 +34,7 @@ export class MovieItemComponent implements OnInit {
   }
 
   onBookBtnClick(movie, theatre) {
-    this.bookBtnClick.emit({movie, theatre});
+    this.bookBtnClick.emit({ movie, theatre });
   }
 
 }
--- src/app/movie/components/movie-list/movie-list.component.ts
+++ src/app/movie/components/movie-list/movie-list.component.ts
@@ -1,5 +1,6 @@
+import { BookingService } from './../../services/booking.service';
+import { MovieService } from './../../services/movie.service';
 import { Movie, Booking, Theatre } from './../../../shared/models/';
-import { MOVIES } from './movie-data';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,44 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-  movies: Movie[] = MOVIES;
-  myBookings: Booking[] = [];
+  movies: Movie[];
+  myBookings: Booking[];
 
-  constructor() { }
+  constructor(
+    private movieService: MovieService,
+    private bookingService: BookingService  
+  ) { }
 
   ngOnInit(): void {
-
+    this.movies = this.movieService.getMovies();
+    this.myBookings = this.bookingService.getBookings();
   }
 
   rateUp(movie: Movie) {
-    if (movie.rating < 5) {
-      movie.rating++;
-    }
+    this.movieService.rateUp(movie);
   }
 
   rateDown(movie: Movie) {
-    if (movie.rating > 1) {
-      movie.rating--;
-    }
+    this.movieService.rateDown(movie);
   }
 
   book(eventData: any) {
     let { movie, theatre }: { movie: Movie, theatre: Theatre } = eventData;
-    let newBooking: Booking = this.myBookings.find(booking => booking.movieId === movie.id && booking.theatreId === theatre.id);
-
-    if (newBooking) {
-      newBooking.qty++;
-    } else {
-      newBooking = {
-        movieId: movie.id,
-        movie: movie.name,
-        theatreId: theatre.id,
-        theatre: theatre.name,
-        price: theatre.ticket_price,
-        qty: 1
-      };
-      this.myBookings.push(newBooking);
-    }
+    this.bookingService.addBooking(movie, theatre);
   }
 
 }
