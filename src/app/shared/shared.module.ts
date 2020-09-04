f6b728ed915b634c8aa73e497477705bc1661356
Kumar Abhinav <abhinav0441@gmail.com>
2020-09-04 13:29:02 +0530

    unless structural directives

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,18 +5,21 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RatingPipe } from './pipes/rating.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
+import { UnlessDirective } from './directives/unless.directive';
 
 @NgModule({
   declarations: [
     NavBarComponent,
     RatingPipe,
-    HighlightDirective
+    HighlightDirective,
+    UnlessDirective
   ],
   imports: [CommonModule, FormsModule],
   exports: [
     NavBarComponent,
     RatingPipe,
-    HighlightDirective
+    HighlightDirective,
+    UnlessDirective
   ]
 })
 export class SharedModule { }
