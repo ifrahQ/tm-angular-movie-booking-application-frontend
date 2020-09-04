82a158568440df036b2d4c26368995665ae322da
Kumar Abhinav <abhinav0441@gmail.com>
2020-09-04 13:26:18 +0530

    highlight directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,16 +4,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { RatingPipe } from './pipes/rating.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    RatingPipe
+    RatingPipe,
+    HighlightDirective
   ],
   imports: [CommonModule, FormsModule],
   exports: [
     NavBarComponent,
-    RatingPipe
+    RatingPipe,
+    HighlightDirective
   ]
 })
 export class SharedModule { }
